80b7c2fb33a6a22ea51198d705f60ad74cf32219
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-11 17:18:08 +0800

    Update 数组倒存输出.c

--- 数组倒存输出.c
+++ 数组倒存输出.c
@@ -5,14 +5,14 @@ int main()
     char a[10];
     int i;
     for(i = 0;i <=9;i++)
-        scanf("%c",&a[i]);
+        scanf("%c",&a[i]);             //正序输入
     for(i = 0;i <= 4;i++)
         {
         a[i] = a[9 - i] + a[i];
         a[9 - i] = a[i] - a[9 - i];
         a[i] = a[i] - a[9 - i];
-        }
+        }                              //倒序
     for(i = 0;i <= 9;i++)
-        printf("%c",a[i]);
+        printf("%c",a[i]);             //输出
     return 0;
 }
