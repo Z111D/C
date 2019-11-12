a7d6cec04cebf6357a23eff1ec67904ca5394cc1
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-12 12:47:43 +0800

    Update 把字符串中的所有字符按从小到大的顺序排序 .c

--- 把字符串中的所有字符按从小到大的顺序排序 .c
+++ 把字符串中的所有字符按从小到大的顺序排序 .c
@@ -3,10 +3,13 @@
 int main()
 {
     int i,j,length = 0;
-    char a[20],temp;
-    char *p = a;
+    char a[20];     /*用于存放字符串*/
+    char temp;      /*定义一个中间变量temp,用于交换字符的位置*/
+    char *p = a;    /*将指针p指向a*/
+    /*输入要排序的字符串*/
     gets(p);
-    length=strlen(p);
+    length=strlen(p);   /*计算字符串的长度*/ 
+    /*将字符串中的字符按从小到大的顺序进行比较*/
     for(i = 0;i < length - 1;i++)
     {
         for(j = i + 1;j < length;j++)
@@ -17,6 +20,7 @@ int main()
             p[j] = temp;
         }
     }
+    /*输出排序后的字符串*/
     puts(p);
     return 0;
 }
