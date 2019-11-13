733ab2af92ae7589b7c67eeeb07d54d2281b8318
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-13 21:21:25 +0800

    Update 字符串逆序.c

--- 字符串逆序.c
+++ 字符串逆序.c
@@ -3,13 +3,13 @@ int main()
 {
 	int i, j, cnt;
 	char ch, a[80], b[80];
-	ch=getchar();
-	for(i=0; ch!='\n'; i++){
-		a[i]=ch;
+	ch = getchar();
+	for(i = 0; ch != '\n'; i++){
+		a[i] = ch;
 		cnt++;
-		ch=getchar();
+		ch = getchar();
 	}
-	for(i=0, j=cnt-1; i<cnt; i++, j--)
+	for(i=0, j=cnt-1; i<cnt; i++, j--)	//i 从零开始所以为i < cnt
 			b[j]=a[i];
 	for(i=0; i<cnt; i++)
 		printf("%c", b[i]);
