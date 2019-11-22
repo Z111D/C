0da23d248f8c8fdd9eb743b93b6980ad6fee156b
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-22 17:58:18 +0800

    Update and rename 1! + 2! + ... +15!.c to 1! + 2! + ... +n!.c

--- 1! + 2! + ... +n!.c
+++ 1! + 2! + ... +n!.c
@@ -2,12 +2,12 @@
 int main()
 {
     int sum,i,N,num;
-    sum = 0;
+    sum = 0;        //和为0
     num = 1;
     scanf("%d",&N);
     for(i = 1;i <= N;i++)
     {
-        num = num * i;
+        num = num * i;      //要加的数
         sum = sum + num;
     }
     printf("%d",sum);
