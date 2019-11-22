100b6a232f898c14cc3cb875ca213a3b62304ce7
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-22 18:02:16 +0800

    Update 100-1000所有质数.c

--- 100-1000所有质数.c
+++ 100-1000所有质数.c
@@ -4,19 +4,19 @@ int main()
 {
     int prime,i,j,k;
     prime = 0;
-    for(i = 100;i <= 1000;i++)
+    for(i = 100;i <= 1000;i++)          //100 - 1000 所有的数
     {
         k = sqrt(i);
         for(j = 2;j <= k;j++)
         {
-            if(i%j == 0)break;
+            if(i%j == 0)break;          //不是质数
 
         }
-        if(j > k)
+        if(j > k)                   //是质数
         {
             prime++;
             printf("%d\t",i);
-            if(prime%5 == 0)printf("\n");
+            if(prime%5 == 0)printf("\n");       //5个一行输出
         }
     }
 return 0;
