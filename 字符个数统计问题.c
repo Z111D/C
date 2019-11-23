96d5bc6f2832f762ca0d9c75c15158b18a4a10bb
Z111D <57184694+Z111D@users.noreply.github.com>
2019-11-24 07:54:15 +0800

    Update 字符个数统计问题.c

--- 字符个数统计问题.c
+++ 字符个数统计问题.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<ctype.h>
+#include<string.h>
 int main()
 {
-    char a[99];
+    char a[100];
     int i,alpha = 0,digit = 0,other = 0;
-    for(i = 0;i < 99;i++)
+    gets(a);
+    for(i = 0;i < (int)strlen(a);i++)
     {
-        a[i] = getchar();
-        if(a[i] == '\n')break;
-        else if(isdigit(a[i]) != 0)digit++;
-             else if(isalpha(a[i]) != 0)alpha++;
-                  else other++;
+        if(isdigit(a[i]) != 0)digit++;
+        else if(isalpha(a[i]) != 0)alpha++;
+             else other++;
     }
     printf("%d %d %d",alpha,digit,other);
     return 0;
