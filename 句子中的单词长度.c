45a83271ca06f8450e809101107b8df9b1f9b18f
Z111D <57184694+Z111D@users.noreply.github.com>
2019-12-24 16:43:13 +0800

    Add files via upload

---
+++ 句子中的单词长度.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char ch;
+    char sign = ' ';
+    int len;
+    int flag;
+    len = 0;
+    flag = 0;// 0:前面无词；  1：前面有词
+    while( (ch=getchar())!='.')
+    {
+        if(ch!=' ') len++;
+        else if(len!=0)
+        { //发现一个非空格字符
+            if(flag!=0) putchar(sign); //如果有词
+            flag = 1;
+            printf("%d",len);
+            len = 0;
+        }
+        else continue;
+    }
+    if(flag==1 && len!=0) putchar(sign);
+    if (len) printf("%d",len);
+    return 0;
+}
