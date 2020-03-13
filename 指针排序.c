527d0474896c99ac7f513b3d25b5c1c995104013
Z111D <57184694+Z111D@users.noreply.github.com>
2020-03-13 18:13:52 +0800

    Add files via upload

--- 指针排序.c
+++ 指针排序.c
@@ -1,25 +1,30 @@
-#include<stdio.h>
-int main()
-{
-    int i,j,k,t,a[10];
-    int *p = a;
-    for(i = 0;i < 10;i++)
-        scanf("%d",&*(a + i));
-    for(i = 0;i < 9;i++)
-    {
-        k = i;
-        for(j = i + 1;j < 10;j++)
-        {
-            if(*(a + k) > *(a + j))k = j;
-        }
-        if(k != i)
-        {
-            t = *(a + i);
-            *(a + i) = *(a + k);
-            *(a + k) = t;
-        }
-    }
-    for(i = 0;i < 10;i++)
-        printf("%d\t",*(a + i));
-    return 0;
+#include "stdio.h"
+#include <stdlib.h>
+int main(int argc,char *argv[]){
+	double *p,t;
+	int n,i,j,k;
+	printf("Enter n(int 1<n<11)...\nn=");
+	if(scanf("%d",&n)!=1 || n<2 || n>10)
+	{//输入n,输错了退出
+		printf("Input error, exit...\n");
+		return 0;
+	}
+	if((p=(double *)malloc(sizeof(double)*n))==NULL){//申请内存存放n个实数
+		printf("Application memory failure, exit...\n");//申请失败退出
+		return 0;
+	}
+	printf("Please enter the %d real Numbers...\n",n);
+	for(i=0;i<n;scanf("%lf",p+i++));//输入n个实数存入数组
+		printf("Results:\n");
+	for(i=0;i<n;i++){//由大到小排序并输出
+		for(k=i,j=k+1;j<n;j++)
+			if(*(p+k)<*(p+j))
+				k=j;
+		if(k-i)
+			t=*(p+k),*(p+k)=*(p+i),*(p+i)=t;
+		printf(i ? " %.2f" : "%.2f",*(p+i));
+	}
+	free(p);
+	putchar('\n');
+	return 0;
 }
