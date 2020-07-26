e0dbd217d3e79cae21f544eebf72116e32b40f32
Z111D <57184694+Z111D@users.noreply.github.com>
2020-07-26 15:54:57 +0800

    Add files via upload

---
+++ Exercise4-13.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+void reverse(char s[])
+{
+	void reverser(char s[], int i, int len);
+	reverser(s, 0, strlen(s));
+}
+
+void reverser(char s[], int i, int len)
+{
+	int c,j;
+	j = len - (i + 1);
+	if(i < j)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+		reverser(s, ++i, len);
+	}
+}
+
+main()
+{
+	char str[100];
+	scanf("%s",str);
+	reverse(str);
+   	printf("%s",str);
+}
